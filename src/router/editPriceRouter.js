ff538b975c65eb7169bb2459c7ee8ce0e853f4cd
Zegendary <zxw1992513@qq.com>
2017-11-29 23:22:15 +0800

    gh-page

--- src/router/editPriceRouter.js
+++
@@ -1,7 +0,0 @@
-export default [{
-  path: '/editPrice/index',
-  component: () => import('@/components/EditPrice/Index')
-},{
-  path: '/editPrice/edit',
-  component: () => import('@/components/EditPrice/EditPrice')
-}]
